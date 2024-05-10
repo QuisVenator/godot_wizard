7a0e838309bf9412d1a0793ea666beed6e008b8b
Manuel René Pauls Toews <paulsmrene@live.com>
2024-05-10 01:23:09 -0400

    Cards fill hand from center

--- Playarea.gd
+++ Playarea.gd
@@ -4,12 +4,8 @@ const card_base = preload("res://assets/cards/card_base.tscn")
 const hand = preload("res://PlayerHand.gd")
 const card_database = preload("res://assets/cards/CardDatabase.gd")
 
-@onready var hand_center = Vector2(get_viewport().size) * Vector2(0.5, 2.65)
 
 # rad1 = card.size[0] / 2
-@onready var rad2 = get_viewport().size[1] * 4
-var angle = -PI / 2 - PI / 150 * 9
-var card_rotation = - PI / 300 * 9
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,11 +20,4 @@ func _input(event):
 	if Input.is_action_just_pressed("leftclick"):
 		var card = card_base.instantiate().setup((randi_range(3, 53)))
 
-		# The position of the card is set on the elipse formed by rad1 = card.size[1] / 2 and rad2, centered at hand_center
-		card.position = Vector2(hand_center.x + rad2 * cos(angle), hand_center.y + card.size[1] * 8 * sin(angle))
-		card.position -= (card.size / 2).rotated(card_rotation)
-		card.rotation = card_rotation
-		angle += PI / 150
-		card_rotation += PI / 300
-		$PlayerHand.add_child(card)
-		print("Card added to scene.")
+		$PlayerHand.add_card(card)
--- PlayerHand.gd
+++ PlayerHand.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var hand_center = Vector2(get_viewport().size) * Vector2(0.5, 2.65)
+@onready var rad2 = get_viewport().size[1] * 4
+
+var cards = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,3 +25,33 @@ func highlight(card):
 func unhighlight():
 	for c in get_children():
 		c.set_modulate(Color(1, 1, 1))
+
+func add_card(card):
+	cards.append(card)
+	arrange_cards()
+	add_child(card)
+	print("Card added to scene.")
+
+func remove_card(card):
+	cards.erase(card)
+	remove_child(card)
+	print("Card removed from scene.")
+
+func arrange_cards():
+	var num_cards = cards.size()
+	var per_card_angle = PI / 150
+	var per_card_rotation = PI / 300
+	var cards_to_left = num_cards / 2.0 - 0.5
+
+	if num_cards == 0:
+		return
+	
+	var angle = -PI / 2 - per_card_angle * cards_to_left
+	var card_rotation = - per_card_rotation * cards_to_left
+
+	for card in cards:
+		card.position = Vector2(hand_center.x + rad2 * cos(angle), hand_center.y + card.size[1] * 8 * sin(angle))
+		card.position -= (card.size / 2).rotated(card_rotation)
+		card.rotation = card_rotation
+		angle += per_card_angle
+		card_rotation += per_card_rotation
